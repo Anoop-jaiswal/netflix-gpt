23908a0a00bf26a3f803a504bf35a40419210911
Anoop Jaiswal <AJaiswal@ESTUATE.COM>
2024-07-03 17:35:21 +0530

    header added

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/335ddde7-3955-499c-b4cc-ca2eb7e1ae71/a7d20bc1-831c-4f9d-8153-11bdf7a08d23/IN-en-20240624-POP_SIGNUP_TWO_WEEKS-perspective_WEB_13cda806-d858-493e-b4aa-f2792ff965dc_medium.jpg"
+          alt="logo"
+        />
+      </div>
+      <form className="relative">
+        <input type="text" placeholder="Email adress" className="p-2 m-2" />
+        <input type="password" placeholder="Password" className="p-2 m-2" />
+        <button className="p-4 m-4">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
