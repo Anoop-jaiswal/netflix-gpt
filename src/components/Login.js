dc95b90e07f6fd76ae8d7c7003defea43f43066e
ANOOP <connect.anoopjaiswal@gmail.com>
2024-07-04 07:28:41 +0530

    signIn/signUp form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const handleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
   return (
     <div>
       <Header />
@@ -11,10 +15,33 @@ const Login = () => {
           alt="logo"
         />
       </div>
-      <form className="relative">
-        <input type="text" placeholder="Email adress" className="p-2 m-2" />
-        <input type="password" placeholder="Password" className="p-2 m-2" />
-        <button className="p-4 m-4">Sign In</button>
+      <form className="p-10 w-3/12 absolute my-36 mx-auto right-0 left-0 bg-black py-20 text-white bg-opacity-80">
+        <h1 className="text-3xl py-4">{isSignIn ? "Sign In" : "Sign Up"}</h1>
+        {!isSignIn && (
+          <input
+            type="text"
+            placeholder="Full name"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email address"
+          className="p-4 my-2 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-2 w-full  bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-900 w-full rounded-lg">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="p-2 cursor-pointer" onClick={handleSignIn}>
+          {isSignIn
+            ? "New to Netflix? Sign up now."
+            : "Already registerd? Sign In now."}
+        </p>
       </form>
     </div>
   );
