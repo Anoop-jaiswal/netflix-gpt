71b1144648ec87a259ea4f9ed28a793ea0c1c447
Anoop Jaiswal <AJaiswal@ESTUATE.COM>
2024-07-08 12:37:53 +0530

    embedded youtube video and make it autoplay

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[18%] px-12 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="">
-        <button className="bg-gray-700 text-white text-xl py-2 px-12 opacity-60 rounded-md">
+        <button className="bg-white text-black text-xl py-2 px-12 opacity-70 rounded-md hover:opacity-60">
           ▶ Play
         </button>
-        <button className="bg-gray-700 text-white text-xl py-2 px-12 opacity-60 rounded-md mx-2">
+        <button className="bg-white text-black text-xl py-2 px-12 opacity-70 rounded-md mx-2 hover:opacity-60">
           More info
         </button>
       </div>
