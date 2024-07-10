2612c29a2d9799928458f9c29e8087732043e464
Anoop Jaiswal <AJaiswal@ESTUATE.COM>
2024-07-10 21:11:05 +0530

    integrating openAI

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useRef } from "react";
+import openai from "../utils/openai";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addGptMoviesResults } from "../redux/gptSearchSlice";
 
 const GptSearchBar = () => {
+  const searchText = useRef(null);
+  const dispatch = useDispatch();
+
+  //This function will take and search movie in TMDB database
+  const searchMovieTmdb = async (movieName) => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`,
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+
+  const handleGptSearch = async () => {
+    const gptQuery =
+      "Act as a movie recomendation system and suggets some movie for the query :" +
+      searchText.current.value +
+      ". only give me a name of 5 movies, comma separated like the example result given ahead. Example Result : gadar,sholay,golmal,koi mil gya,don";
+
+    try {
+      const gptResponse = await openai.chat.completions.create({
+        messages: [{ role: "user", content: gptQuery }],
+        model: "gpt-3.5-turbo",
+      });
+      const getMoviesName = gptResponse.choices[0]?.message?.content.split(",");
+
+      //search for each movie, this will return array of promise
+      const promiseDataArray = getMoviesName.map((movie) =>
+        searchMovieTmdb(movie)
+      );
+      const resultTmdb = await Promise.all(promiseDataArray);
+      dispatch(addGptMoviesResults(resultTmdb));
+    } catch (error) {
+      console.log(error, "eerpr");
+    }
+  };
+
   return (
     <div className="pt-[8%] flex justify-center">
-      <form className=" bg-black w-1/2 grid grid-cols-12">
+      <form
+        className=" bg-black w-1/2 grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           className="p-4 m-4 col-span-9"
           type="text"
           placeholder="what do you want to watch today"
         />
-        <button className="col-span-3 py-2 px-4 bg-red-700 text-white rounded-md m-4">
+        <button
+          onClick={handleGptSearch}
+          className="col-span-3 py-2 px-4 bg-red-700 text-white rounded-md m-4"
+        >
           Search
         </button>
       </form>
--- src/redux/gptSearchSlice.js
+++ src/redux/gptSearchSlice.js
@@ -4,14 +4,18 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     showGptSearch: false,
+    gptMovies: null,
   },
   reducers: {
     toggleGptSearchView: (state, action) => {
       state.showGptSearch = !state.showGptSearch;
     },
+    addGptMoviesResults: (state, action) => {
+      state.gptMovies = action.payload;
+    },
   },
 });
 
-export const { toggleGptSearchView } = gptSlice.actions;
+export const { toggleGptSearchView, addGptMoviesResults } = gptSlice.actions;
 
 export default gptSlice.reducer;
